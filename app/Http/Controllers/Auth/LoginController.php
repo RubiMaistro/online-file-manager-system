f25bb6a0717fd6ebcd65a6963364cd056bffee0f
Deák Ruben <rubendeak15@gmail.com>
2021-11-30 11:53:09 +0100

    Registration new param: username, login with username and password

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -36,5 +36,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Authenticate user 
+     * 
+     * @return string
+     */
+    public function username() {
+        return 'username';
     }
 }
