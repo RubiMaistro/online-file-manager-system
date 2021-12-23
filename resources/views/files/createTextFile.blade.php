6bae429bac5a9f2f824f36aa95c918d0b7672bee
Deák Ruben <rubendeak15@gmail.com>
2021-12-23 14:11:32 +0200

    Error message in create text manager

--- resources/views/files/createTextFile.blade.php
+++ resources/views/files/createTextFile.blade.php
@@ -4,22 +4,19 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card" style="background-color:lightsalmon">
+            <div class="card" style="background-color:lightsalmon; font-weight:bold">
                 <div class="card-header" style="font-size: x-large; text-align: center">{{ __('Create Text File Manager') }}</div>
                 <div class="card-body" style="font-size: large;">
+                    <div>
+                        <label style="color: red">{{ $notEnteredErr }}</label>
+                    </div>
                     <form method="POST" action="{{ url('/file/create/text') }}" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group row">
                             <label for="name" class="col-md-2 col-form-label text-md-right">{{ __('File Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name it your file">
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" autocomplete="name" autofocus placeholder="Name it your file">
                             </div>
                         </div>
                         <div class="form-group row">
